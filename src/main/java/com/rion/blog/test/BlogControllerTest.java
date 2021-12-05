585a554a769f3b728bcf34a0ae3a4c7e1b3dbcfa
Jeonsangyeon2304 <wjstkddus01@gmail.com>
2021-12-06 04:34:02 +0900

    Set Project Base

---
+++ src/main/java/com/rion/blog/test/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.rion.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String hello() {
+        return "<H1>Hello, World!!!</H1>";
+    }
+}
---
+++ src/test/java/com/rion/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rion.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
